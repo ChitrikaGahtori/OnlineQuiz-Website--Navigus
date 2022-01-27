c1c5d5d231555d6687b3bc8b09cbc6d14938b2b1
ChitrikaGahtori <74341338+ChitrikaGahtori@users.noreply.github.com>
2022-01-27 15:13:07 +0530

    Update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -23,7 +23,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
+# SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
