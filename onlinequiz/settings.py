8c04af6f75ae763f595ae771c09afa6e3deb365d
ChitrikaGahtori <74341338+ChitrikaGahtori@users.noreply.github.com>
2022-01-27 13:57:50 +0530

    Update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -131,7 +131,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles');
 
 LOGIN_REDIRECT_URL='/afterlogin'
