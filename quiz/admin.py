bc853d8640e79fa7c6cb8a29bc94e66e85a1fae5
ChitrikaGahtori <74341338+ChitrikaGahtori@users.noreply.github.com>
2022-01-27 12:38:46 +0530

    Online Quiz

---
+++ quiz/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from quiz.models import *
+
+
+admin.site.register(Course)
+admin.site.register(Question)
+admin.site.register(Result)
